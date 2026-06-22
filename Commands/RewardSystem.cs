a31d77149e8f5b3ee5bbe0716f5a39f72253d9de
Sonic <mcand707@yahoo.com>
2026-06-21 20:55:52 -0500

    Mm..

--- Commands/RewardSystem.cs
+++ Commands/RewardSystem.cs
@@ -60,7 +60,7 @@ public class RewardSystem {
         if (item == 1) {
             embedBuilder
                 .WithAuthor("Dear Prospect, you have been awarded . . .")
-                .WithTitle("TRIAL HEADPHONES")
+                .WithTitle("【 TRIAL HEADPHONES 】")
                 .WithThumbnailUrl("https://64.media.tumblr.com/bca2cc2f79769603271b08c771604ff8/e0c197076de26732-2b/s540x810/6ba9914651ae690969cd4c896a9f566527fa90a0.pnj")
                 .WithDescription("These headphones are your first step towards enlistment.\n\n0 1  ∥  040506 | LOCKED\n0 2  ∥  222f2f | LOCKED\n0 3  ∥  0d0f12 | LOCKED")
                 .WithImageUrl("https://64.media.tumblr.com/3477f610c193e36e7dec89cdd4950dc9/e0c197076de26732-d9/s1280x1920/77b23df058976d98915498920e55feb130d55470.pnj")
@@ -69,7 +69,7 @@ public class RewardSystem {
         } else if (item == 2) {
             embedBuilder
                 .WithAuthor("Dear Prospect, you have been awarded . . .")
-                .WithTitle("TRIAL SWORD")
+                .WithTitle("【 TRIAL SWORD 】")
                 .WithThumbnailUrl("https://64.media.tumblr.com/0af3a5ffef90e7bd73441f53fff50d8a/e0c197076de26732-f7/s540x810/0a2a680a38dd2882a33e6acd700b0849ac5b9d80.pnj")
                 .WithDescription("You've made it past your mandatory lectures! Good going.\nYou can attend Downtown Patrols and Scoutings now that you have this sword in hand.\n\n0 1  ∥  08090a | 192126\n0 2  ∥  151618 | LOCKED\n0 3  ∥  260c0c | 08090a\n0 4  ∥  504029 | 08090a")
                 .WithImageUrl("https://64.media.tumblr.com/f4e68447a8cc190693797e5563c51d1b/e0c197076de26732-c8/s1280x1920/8b27f009674e970061ce55f34d35e6c136b17d00.pnj")
@@ -78,7 +78,7 @@ public class RewardSystem {
         } else if (item == 3) {
             embedBuilder
                 .WithAuthor("Dear Prospect, you have been awarded . . .")
-                .WithTitle("PARADE DRESS")
+                .WithTitle("【 PARADE DRESS 】")
                 .WithThumbnailUrl("https://64.media.tumblr.com/9c0494481ed62afa3249222b17ae5610/e0c197076de26732-f5/s2048x3072/b325d552629a22c6c61896c9fc83c03311e0e7d7.pnj")
                 .WithDescription("Wow! Take a look at you, finally ready to move up in the ranks!\nYou'll have to complete this uniform and submit it in order to attend the upcoming enlistment ceremony.\nPlease be on time!\n\n[. . PRE-ENLISTED PARADE DRESS . .](<https://docs.google.com/document/d/1mQN15_Rxn1VtBBWSjVgqo8x1_zpSh0iDTtrfbcvyNNk/edit?tab=t.0>)")
                 .WithImageUrl("https://64.media.tumblr.com/616bce1d6e1a6d7a2123c76d6f249404/2ecded076fd064e9-c6/s1280x1920/11d327bf242c41a07ca122757d050c6c6ce52da1.pnj")
