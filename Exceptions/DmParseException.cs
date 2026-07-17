b6ff841a4c0c22cac18212c4e468f9102f24e71e
Sonic <mcand707@yahoo.com>
2026-07-16 22:53:17 -0500

    Yeah, man.

---
+++ Exceptions/DmParseException.cs
@@ -0,0 +1,10 @@
+﻿namespace SMASSB.Exceptions;
+
+public class DmParseException : Exception {
+    
+    public string UserName { get; }
+    
+    public DmParseException(string userName) : base($"Failed to parse message in '{userName}'s DMs'.") {
+        UserName = userName;
+    }
+}
