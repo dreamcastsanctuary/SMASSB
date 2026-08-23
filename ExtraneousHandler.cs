63152ac30ac78f20af01b48023c515600662f9e8
Sonic <mcand707@yahoo.com>
2026-08-22 23:55:07 -0500

    They're BEATING MY ASS for this change.

--- ExtraneousHandler.cs
+++ ExtraneousHandler.cs
@@ -459,7 +459,7 @@ public class ExtraneousHandler {
     public async Task ButtonHandler(SocketMessageComponent component) {
 
         var id = component.Data.CustomId;
-        var guild = _client.GetGuild((ulong)component.GuildId);
+        var guild = _client.GetGuild((1471660035854569505));
 
         if (id.StartsWith("buy_item_")) {
             try {
