e7430d2fe8f05cbaa91bb0a97fecd35ab8f152de
Sonic <mcand707@yahoo.com>
2026-09-04 15:55:39 -0500

    Repositioned Avatar on IDs.

--- Commands/IdSystem.cs
+++ Commands/IdSystem.cs
@@ -96,7 +96,7 @@ public class IdSystem {
         }));
         
         var namePos = new Point(827, 452);
-        var avatarPos = new Point(93,369);
+        var avatarPos = new Point(93,373);
         var idPos = new Point(1219,154);
         var datePos = new Point(827,531);
         var rankPos = new Point(827,609);
