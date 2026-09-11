9281e42be939dc490e530a4e7be8ed66d227cdfb
Sonic <mcand707@yahoo.com>
2026-09-10 22:37:37 -0500

    DEBUGGGGGG

--- ServiceHandlers/CommandHandler.cs
+++ ServiceHandlers/CommandHandler.cs
@@ -206,7 +206,14 @@ public class CommandHandler {
 
         commands.Add(new SlashCommandBuilder()
             .WithName("id")
-            .WithDescription("Shows or edits your Enlisted ID (or another member's if specified).")
+            .WithDescription("Shows or edits your Enlisted ID.")
+            .AddOption("avatar_url", ApplicationCommandOptionType.String, "The profile of the member / character (optional)", isRequired: false)
+            .AddOption(bloodtypeOption)
+            .AddOption(idTypeOption));
+        
+        commands.Add(new SlashCommandBuilder()
+            .WithName("debugid")
+            .WithDescription("Shows or edits another member's Enlisted ID.")
             .AddOption("member", ApplicationCommandOptionType.User, "The member to view/edit (optional)", isRequired: false)
             .AddOption("avatar_url", ApplicationCommandOptionType.String, "The profile of the member / character (optional)", isRequired: false)
             .AddOption(bloodtypeOption)
@@ -626,6 +633,9 @@ public class CommandHandler {
                 break;
 
             case "id":
+                await _idSystem.HandleIdCommand(command);
+                break;
+            case "debugid":
                 await _idSystem.HandleIdCommand(command);
                 break;
             case "addidaddons":
