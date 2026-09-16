a8488cf2f0570f4b3139ce699feca5e468f2a0df
Sonic <mcand707@yahoo.com>
2026-09-16 15:23:52 -0500

    ok lets try

--- Commands/CellSystem.cs
+++ Commands/CellSystem.cs
@@ -135,14 +135,13 @@ public class CellSystem {
     }
 
     public async Task HandleLaunchEmulatorJs(SocketMessageComponent component, ulong ownerId, string game) {
-
-        await component.DeferAsync();
         
         if (component.User.Id != ownerId) {
-            await component.FollowupAsync("This isn't your cell! You like touching things that don't belong to you?", ephemeral: true);
+            await component.RespondAsync("This isn't your cell! You like touching things that don't belong to you?", ephemeral: true);
             return;
         }
         
+        await component.DeferAsync();
         _db.SetPendingGame(ownerId.ToString(), game);
 
         var payload = new { type = 12 };
